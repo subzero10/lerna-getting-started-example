493d42f67e849db78e40e9e3e44baa609637c079
Pangratios Cosma <pagcosma@gmail.com>
2023-05-26 11:56:12 +0300

    feat(remixapp)!: not really a breaking change v2

--- packages/remixapp/app/root.tsx
+++ packages/remixapp/app/root.tsx
@@ -23,6 +23,7 @@ export default function App() {
       </head>
       <body>
         NOT REALLY A BREAKING CHANGE
+        ANOTHER NOT BREAKING CHANGE
         <Outlet />
         <ScrollRestoration />
         <Scripts />
