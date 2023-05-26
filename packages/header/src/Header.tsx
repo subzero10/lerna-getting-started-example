d4a76249077319b3fb30d5e3f32bc80548e424b3
Pangratios Cosma <pagcosma@gmail.com>
2023-05-26 11:58:07 +0300

    fix(header): a dummy fix

--- packages/header/src/Header.tsx
+++ packages/header/src/Header.tsx
@@ -4,6 +4,7 @@ export function Header() {
     return (
         <header>
             Lerna is the original monorepo tool!
+            (simulating a fix to a dependency package)
         </header>
     );
 }
