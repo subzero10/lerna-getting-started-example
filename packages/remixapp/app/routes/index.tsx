64d17b67e42c666549b93e5960e0a2defbcebefd
Pangratios Cosma <pagcosma@gmail.com>
2023-05-26 11:37:39 +0300

    chore: wip

--- packages/remixapp/app/routes/index.tsx
+++ packages/remixapp/app/routes/index.tsx
@@ -1,5 +1,5 @@
-import {Header} from 'header';
-import {Footer} from 'footer';
+import {Header} from '@hb-test/header';
+import {Footer} from '@hb-test/footer';
 
 export default function Index() {
     return (
